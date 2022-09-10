1f42a02602d30805552b280c9c34554afdd047f8
Matthew Oro <MatthewEOro@gmail.com>
2022-09-09 21:31:16 -0400

    init test commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,15 @@
 # Creating shortcuts for my apps
 source ~/.config/fish/alias.fish
 
+# Some setting (will make external file to keep organized)
+set -g theme_nerd_fonts yes
 
 #bobthefish theme
-#set theme_color_scheme dracula
-set theme_color_scheme terminal-dark
+set theme_color_scheme dracula
+#set theme_color_scheme terminal-dark
 
 
 # Gotham Shell
-eval sh $HOME/.config/gotham/gotham.sh
+#eval sh $HOME/.config/gotham/gotham.sh
 
 fish_add_path /opt/homebrew/sbin
